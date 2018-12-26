51ec149814efe03e3eaf0b63f5ca03056095810a
yml <y@yml.red>
2018-12-26 16:06:59 +0800

    进一步完善基本框架

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models.Administrator import Administrator
 from config import app, db
+import views_auth
 
 
 @app.route('/')
---
+++ views_auth.py
@@ -0,0 +1,25 @@
+from decorators import *
+from config import *
+
+
+@app.route('/secret_page')
+@login_required
+def secret_page():
+    pass
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] == 'yml':  # 登录成功
+            session['user'] = {'uname': request.form['username'],
+                               'pswd': request.form['password']}
+            return render_template('index.html')
+        else:  # 登录失败
+            return render_template('login.html', err_msg=request.form['username'])
+    else:
+        if 'user' in session:
+            return render_template('index.html')
+        else:
+            return render_template('login.html')
