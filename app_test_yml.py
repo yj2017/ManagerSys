6749a065907cc7c8d03f83d37bee39ad3b3e355f
yml <y@yml.red>
2018-12-26 20:45:02 +0800

    注册,登录

--- app_test_yml.py
+++ app_test_yml.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
 from flask import request as req
-from models.Administrator import Administrator
 from config import app, db
 from models.User import User
-import views_auth
+import views_user
 from decorators import *
 
 @app.route('/test')
@@ -20,16 +19,9 @@ if __name__ == '__main__':
     db.create_all()  # 生成数据库表结构
 
     # 在这里写测试代码
-    admin = Administrator(1, 'yml', '123456')
-    print(admin.uname)
-    user = User()
-    print(user.birthday)
-    db.session.add(user)
-    db.session.commit()
 
-    print(User.query.first())
 
     # 测试代码结束
     app.run(debug=True)
-    db.drop_all()  # 删除所有表
+    # db.drop_all()  # 删除所有表
     print('exit.....')
---
+++ views_user.py
@@ -0,0 +1,51 @@
+from decorators import *
+from config import *
+from models.User import User
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        uname = request.form['username']
+        pswd = request.form['password']
+        user = User.query.filter_by(username=uname).first()
+        if user is None:
+            return render_template('login.html', err_msg='用户名不存在！')
+        elif user.password != pswd:
+            return render_template('login.html', err_msg='密码错误！')
+        else:
+            session['user'] = user
+            return render_template('index.html')
+    else:
+        if 'user' in session:
+            return render_template('index.html')
+        else:
+            return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        uname = request.form['username']
+        pswd = request.form['password']
+        rpswd = request.form['rpassword']
+        if pswd == '' or pswd != rpswd:
+            return render_template('register.html', err_msg='两次密码不同！')
+
+        user = User.query.filter_by(username=uname).first()
+        if user is not None:
+            return render_template('register.html', err_msg='用户名已存在！')
+        else:
+            user = User(uname, pswd)
+            db.session.add(user)
+            db.session.commit()
+            return render_template('login.html', err_msg='注册成功！')
+    else:
+        return render_template('register.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    pass
