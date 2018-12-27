9deba25ccfb6141529c6f97f62fe2f9c09091bba
yangjin <1424783824@qqcom>
2018-12-27 23:50:04 +0800

    /

--- app_test_yj.py
+++ app_test_yj.py
@@ -20,16 +20,16 @@ if __name__ == '__main__':
 
     # 在这里写测试代码
 
-    # user1=User('herina','111111',datetime.date(1997,8,27),1,0,'湖北省武汉市武汉理工大学余家头校区','18672019299',0)
-    # user2=User('yj','111111',datetime.date(1997,8,27),1,0,'湖北省武汉市武汉理工大学南湖校区','18672019299',1)
+    user1=User('yangjin','111111',datetime.date(1997,8,27),1,0,'湖北省武汉市武汉理工大学余家头校区','18672019299',0)
+    user2=User('yj','111111',datetime.date(1997,8,27),1,0,'湖北省武汉市武汉理工大学南湖校区','18672019299',0)
     product1=Product(2,'优衣库羽绒服女装',499,'武汉',195,202,'D:\\tomcat9\\webapps\\managerSys\\pictures\\UNIQLO women coat.jpg','UNIQLO','冬装 衣服','填充白鸭绒，材料不错，不会钻毛,三色可以选择')
     product2=Product(2,'优衣库牛仔裤女装',299,'武汉',10,210,'D:\\tomcat9\\webapps\managerSys\\pictures\\women jeans1.jpg','UNIQLO','秋冬 裤子','100%棉')
     product3=Product(2,'纯羊毛帽男女',199,'上海',100,200,'D:\\tomcat9\\webapps\\managerSys\\pictures\\hat1.jpg','UNIQLO','帽','100%羊毛')
     
-    product4=Product(3,'优衣库羽绒服男装',499,'武汉',195,400,'D:\\tomcat9\\webapps\\managerSys\\pictures\\UNIQLO man coat.jpg','UNIQLO','冬装 男装 衣服','填充白鸭绒，材料不错，不会钻毛,三色可以选择')
-    product5=Product(4,'HM男帽秋冬',199,'武汉',195,400,'D:\\tomcat9\\webapps\\managerSys\\pictures\\hat2.jpg','HM','冬装 男帽','100%棉')
-    order1=ProductOrder(1,2,499,datetime.date(2018,12,25),'超级棒',0)
-    order2=ProductOrder(2,2,299,datetime.date(2018,12,25),'便宜又好',1)
+    # product4=Product(3,'优衣库羽绒服男装',499,'武汉',195,400,'D:\\tomcat9\\webapps\\managerSys\\pictures\\UNIQLO man coat.jpg','UNIQLO','冬装 男装 衣服','填充白鸭绒，材料不错，不会钻毛,三色可以选择')
+    # product5=Product(4,'HM男帽秋冬',199,'武汉',195,400,'D:\\tomcat9\\webapps\\managerSys\\pictures\\hat2.jpg','HM','冬装 男帽','100%棉')
+    # order1=ProductOrder(1,2,499,datetime.date(2018,12,25),'超级棒',0)
+    # order2=ProductOrder(2,2,299,datetime.date(2018,12,25),'便宜又好',1)
     # user1=User('herina yang','111111',datetime.date(1997,8,27),0,0,'北京市丰台区','18672019299',0)
     # user2=User('herina','111111',datetime.date(1997,8,27),1,0,'湖北省武汉市武汉大学','18672019299',0)
     
@@ -40,10 +40,7 @@ if __name__ == '__main__':
     db.session.add(product1)
     db.session.add(product2)
     db.session.add(product3)
-    db.session.add(product4)
-    db.session.add(product5)
-    # db.session.add(order1)
-    # db.session.add(order2)
+   
     db.session.commit()
     print(User.query.first())
     
